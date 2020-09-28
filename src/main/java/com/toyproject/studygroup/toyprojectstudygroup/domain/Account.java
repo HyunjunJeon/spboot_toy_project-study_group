88e6e33a6b4c1b18d180cd71a92baeea9348e370
HyunjunJeon <jeonhj920@gmail.com>
2020-09-28 13:20:08 +0900

    project init

---
+++ src/main/java/com/toyproject/studygroup/toyprojectstudygroup/domain/Account.java
@@ -0,0 +1,53 @@
+package com.toyproject.studygroup.toyprojectstudygroup.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Account {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String password;
+
+    private boolean emailVerified; // 이메일 인증 받았는지
+
+    private String emailCheckToken; // 이메일 인증 토큰 저장해서 빠른 비교
+
+    private LocalDateTime joinedAt;
+
+    private String bio; // 자기소개
+
+    private String url; // 개인 웹사이트
+
+    private String occupation; // 직업
+
+    private String location; // 살고있는 지역
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String profileImage;
+    
+    /* 알람 관련 */
+    private boolean studyCreatedByEmail;
+
+    private boolean studyCreatedByWeb;
+
+    private boolean studyEnrollmentResultByEmail;
+
+    private boolean studyEnrollmentResultByWeb;
+
+    private boolean studyUpdateByEmail;
+
+    private boolean studyUpdateByWeb;
+}
---
+++ src/test/java/com/toyproject/studygroup/toyprojectstudygroup/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyproject.studygroup.toyprojectstudygroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
